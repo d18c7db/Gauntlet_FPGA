296fcb49d7031014d61e4f2bd9eab12fe19fb401
Alex <d18c7db@hotmail.com>
2025-04-25 21:20:28 +0200

    Delete unused files

--- MiSTer/rmake.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-import runpy, sys
-sys.argv.append('--root=.')
-sys.argv.append('--core=gauntlet')
-sys.argv.append('--top=tb_gauntlet')
-runpy.run_path('../../replay_common/scripts/common.py')
